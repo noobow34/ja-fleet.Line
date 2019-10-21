ab88060174e3fd90cf302440ea751efd67947a12
noobow34 <skywind.hk34@gmail.com>
2019-10-22 06:15:43 +0900

    ヘルスチェックにDBアクセスを追加

--- ja-fleet.Line/Controllers/CheckController.cs
+++ ja-fleet.Line/Controllers/CheckController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using jafleet.Commons.EF;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace jafleet.Line.Controllers
 {
     public class CheckController : Controller
     {
+        private readonly jafleetContext _context;
+
+        public CheckController(jafleetContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return Content("1");
+            return Content(_context.AircraftView.Count().ToString());
         }
     }
 }
