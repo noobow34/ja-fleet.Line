1a0d8acbe099ba20031c107f2ab9e6b4f58bd999
noobow34 <skywind.hk34@gmail.com>
2020-02-25 23:57:48 +0900

    シートコンフィグ表示

--- ja-fleet.Line/LineBotApp.cs
+++ ja-fleet.Line/LineBotApp.cs
@@ -220,6 +220,7 @@ namespace jafleet.Line
                         $" 製造番号:{av.SerialNumber} \n " +
                         $" 登録年月日:{av.RegisterDate} \n " +
                         $" 運用状況:{av.Operation} \n " +
+                        $" コンフィグ:{av.SeatConfig}\n" +
                         $" WiFi:{av.Wifi} \n " +
                         $" 特別塗装:{av.SpecialLivery} \n " + 
                         $" 備考:{av.Remarks}";
