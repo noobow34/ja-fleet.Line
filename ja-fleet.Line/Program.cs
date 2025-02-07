93232cbe65f745c776d0b7dfc9b90058447192d4
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-02-07 20:29:06 +0900

    トップレベルステートメントに変更

--- ja-fleet.Line/Program.cs
+++ ja-fleet.Line/Program.cs
@@ -1,18 +1,27 @@
-using Microsoft.AspNetCore;
+using jafleet.Commons.EF;
+using jafleet.Line.Middleware;
+using jafleet.Line.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace jafleet.Line
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json").Build();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:6500")
-                .Build();
-    }
-}
+builder.Services.AddDbContextPool<jafleetContext>(
+    options => options.UseNpgsql(config.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddMvc().AddNewtonsoftJson();
+builder.Services.Configure<AppSettings>(config);
+builder.WebHost.UseUrls("http://localhost:6500");
+
+var app = builder.Build();
+
+app.UseLineValidationMiddleware(config.GetSection("LineSettings")["ChannelSecret"]);
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+);
+
+
+app.Run();
